a5fa8992b60472f73b9dc5034c7c39356551d14e
samuelbrowne <sambro2@mybudget.com.au>
2021-10-08 12:55:03 +1030

    Setup initial users component tests

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersComponent } from './users.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,28 @@ describe('UsersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have the correct title', () => {
+    expect(component.title).toBe('Users');
+    expect(Array.isArray(component.allUsers)).toBeTruthy();
+  });
+  it('should create empty users array', () => {
+    expect(component.allUsers.length).toEqual(0);
+    expect(Array.isArray(component.allUsers)).toBeTruthy();
+  });
+  it('should render the correct title', () => {
+    expect(component.allUsers.length).toEqual(0);
+    const h1 = de.query(By.css('h1'));
+    expect(h1.nativeElement.innerText).toBe(component.title);
+    const newTitle = 'AAA';
+    component.title = newTitle;
+    fixture.detectChanges();
+    expect(h1.nativeElement.innerText).toBe(newTitle);
   });
 });
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
-
+  allUsers;
+  title = 'Users';
+  constructor() {
+    this.allUsers = [];
+  }
   ngOnInit(): void {
   }
 
